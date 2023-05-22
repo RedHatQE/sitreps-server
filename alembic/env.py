93679ff424b3a9a55a0a790653500775ab7ed6d9
Nikhil Dhandre <ndhandre@redhat.com>
2023-05-22 18:03:41 +0530

    Fix alembic collection issue.

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import os
 import sys
 from logging.config import fileConfig
@@ -9,11 +10,25 @@ from sqlalchemy import pool
 
 from alembic import context
 
+
 # This allows relative import to work correctly
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from sitreps_server.db import Base
+from sitreps_server.models import (
+    CLOC,
+    CodeCoverage,
+    IntegrationTest,
+    Jira,
+    Metadata,
+    Project,
+    ProjectGroup,
+    Repository,
+    SonarQube,
+    UnitTest,
+)
 
 ENV_FILE = Path(".env")
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,13 +36,13 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-# target_metadata = None
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -48,7 +63,7 @@ def get_url():
     return f"postgresql://{user}:{password}@{server}/{db}"
 
 
-def run_migrations_offline():
+def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -62,14 +77,17 @@ def run_migrations_offline():
     """
     url = get_url()
     context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True, compare_type=True
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
     )
 
     with context.begin_transaction():
         context.run_migrations()
 
 
-def run_migrations_online():
+def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -83,9 +101,8 @@ def run_migrations_online():
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
